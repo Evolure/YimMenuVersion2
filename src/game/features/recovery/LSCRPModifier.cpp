48c26a959a6e8616673fc69fa7bb6c6c114a16a0
Hexlane <deede3771@gmail.com>
2026-02-24 00:05:38 -0500

    Update LSCRPModifier.cpp

--- src/game/features/recovery/LSCRPModifier.cpp
+++ src/game/features/recovery/LSCRPModifier.cpp
@@ -1,7 +1,12 @@
 #include "core/commands/IntCommand.hpp"
 #include "core/commands/Command.hpp"
 #include "game/gta/Stats.hpp"
+#include "game/gta/Natives.hpp"
+#include "core/frontend/Notifications.hpp"
+#include "core/backend/FiberPool.hpp"
 #include <algorithm>
+
+
 namespace YimMenu::Features
 {
 	static IntCommand LSCarMeetRankSlider{
@@ -28,4 +33,38 @@ namespace YimMenu::Features
 	    "applylscarmeetrank",
 	    "Apply LS Car Meet Rank",
 	    "Applies the selected LS Car Meet rank."};
+	//Loaction Needs Changed 
+	class Complete_Challenge : Command
+	{
+		using Command::Command;
+
+		virtual void OnCall() override
+		{
+			Stats::SetBool("MPX_CARMEET_PV_CHLLGE_CMPLT", true);
+			Stats::SetBool("MPX_CARMEET_PV_CLMED", false);
+			Notifications::Show("YimMenuV2", "Done. If you are in LSCM, re-enter to collect the prize.", NotificationType::Success);
+		}
+	};
+	static Complete_Challenge _Complete_Challenge{
+    "Complete_Challenge",
+    "Unlock LSCM Prize Ride",
+    "Unlocks the LS Car Meet prize ride instantly"};
+	//tp
+	class TeleportToLSCM : public Command
+	{
+	 using Command::Command;
+    void OnCall() override
+    {
+        FiberPool::Push([]()
+        {
+            auto blip = HUD::GET_FIRST_BLIP_INFO_ID(777);
+            auto coords = HUD::GET_BLIP_COORDS(blip);
+            PED::SET_PED_COORDS_KEEP_VEHICLE(PLAYER::PLAYER_PED_ID(), coords.x, coords.y, coords.z);
+        });
+    }
+};
+static TeleportToLSCM _TeleportToLSCM{
+    "tp_lscm",
+    "Teleport to LS Car Meet",
+    "Teleport to the LS Car Meet"};
 }
