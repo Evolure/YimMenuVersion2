77cb00d0c7e4d008ef22d2e1a5acd95c6c4c7a23
Voidlane <hexlane98@gmail.com>
2026-03-02 12:34:56 -0500

    Added unlockfps Test

--- src/game/frontend/submenus/Debug.cpp
+++ src/game/frontend/submenus/Debug.cpp
@@ -12,6 +12,13 @@ namespace YimMenu::Submenus
 		#define ICON_FA_ARROW_RIGHT "\xef\x84\xa0"
 	    Submenu::Submenu("Debug", ICON_FA_ARROW_RIGHT)
 	{
+		auto main = std::make_shared<Category>("Main");
+		auto GeneralGroup = std::make_shared<Group>("General");
+
+		GeneralGroup->AddItem(std::make_shared<BoolCommandItem>("unlockfps"_J));
+
+		main->AddItem(GeneralGroup);
+		AddCategory(std::move(main));
 		AddCategory(BuildMiscMenu());
 		AddCategory(BuildGlobalsMenu());
 		AddCategory(BuildLocalsMenu());
