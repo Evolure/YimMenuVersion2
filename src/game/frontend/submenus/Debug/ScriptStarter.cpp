a72f2e4711fdc9db44e43921f833ba09076d38aa
Voidlane <hexlane98@gmail.com>
2026-03-03 11:00:43 -0500

    Update ScriptStarter.cpp

--- src/game/frontend/submenus/Debug/ScriptStarter.cpp
+++ src/game/frontend/submenus/Debug/ScriptStarter.cpp
@@ -5,40 +5,59 @@
 #include "game/gta/ScriptGlobal.hpp"
 #include "core/util/Joaat.hpp"
 #include "game/frontend/items/Items.hpp"
+
 namespace YimMenu::Submenus
 {
-	std::shared_ptr<Category> BuildScriptStarterMenu()
-	{
-		auto menu = std::make_shared<Category>("Script Starter");
-		auto main = std::make_shared<Group>("Scripts");
+    std::shared_ptr<Category> BuildScriptStarterMenu()
+    {
+        auto menu = std::make_shared<Category>("Script Starter");
+        auto main = std::make_shared<Group>("Scripts");
 
-		main->AddItem(std::make_unique<ImGuiItem>([]
-		{
-			if (ImGui::Button("Start Internet"))
-			{
-				FiberPool::Push([]
-				{
-					Utils::StartScript("appInternet", 4592);
-				});
-			}
-			
-			if (ImGui::Button("Start CEO Computer"))
-			{
-				FiberPool::Push([]
-				{
-					Utils::StartScript("appSecuroServ", 1424);
-				});
-			} if (ImGui::IsItemHovered()) {ImGui::SetTooltip("Must be in your CEO office interior. It will damage the screen if you buy a warehouse");}
-			
-			if (ImGui::Button("Start Vehicle Cargo Computer"))
-			{
-				FiberPool::Push([]
-				{
-					Utils::StartScript("appImportExport", 1424);
-				});
-			} if (ImGui::IsItemHovered()){ImGui::SetTooltip("Must be in your vehicle warehouse interior");}
-		}));
-		menu->AddItem(std::move(main));
-		return menu;
-	}
-}//TODO: All businesses
+        main->AddItem(std::make_unique<ImGuiItem>([]
+        {
+            // Internet App
+            if (ImGui::Button("Start Internet"))
+            {
+                FiberPool::Push([] { Utils::StartScript("appInternet", 4592); });
+            }
+
+            // CEO Computer
+            if (ImGui::Button("Start CEO Computer"))
+            {
+                FiberPool::Push([] { Utils::StartScript("appSecuroServ", 1424); });
+            }
+            if (ImGui::IsItemHovered())
+            {
+                ImGui::SetTooltip("Must be in your CEO office interior. Buying a warehouse may damage the screen.");
+            }
+
+            // Vehicle Cargo Computer
+            if (ImGui::Button("Start Vehicle Cargo Computer"))
+            {
+                FiberPool::Push([] { Utils::StartScript("appImportExport", 1424); });
+            }
+            if (ImGui::IsItemHovered())
+            {
+                ImGui::SetTooltip("Must be in your Vehicle Warehouse interior.");
+            }
+
+            if (ImGui::Button("Start Bunker Computer"))
+            {
+                FiberPool::Push([] { Utils::StartScript("appBunkerBusiness", 1424); });
+            }
+
+            // MC Businesses Computer
+            if (ImGui::Button("Start MC Business Computer"))
+            {
+                FiberPool::Push([] { Utils::StartScript("appBikerBusiness", 4592); });
+            }
+            if (ImGui::IsItemHovered())
+            {
+                ImGui::SetTooltip("Must be in your MC Clubhouse. Manage Cocaine, Meth, or other businesses.");
+            }
+        }));
+
+        menu->AddItem(std::move(main));
+        return menu;
+    }
+}//TODO: Add remaining businesses or custom scripts
