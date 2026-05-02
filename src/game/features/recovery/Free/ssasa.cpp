12aa6c29c206ffc1241871d2d5dcf9da1d173c02
Pyxalon <kythera051@gmail.com>
2026-05-02 16:47:31 -0400

    Create ssasa.cpp

---
+++ src/game/features/recovery/Free/ssasa.cpp
@@ -0,0 +1,15 @@
+#include "core/commands/Command.hpp"
+#include "game/gta/Stats.hpp"
+namespace YimMenu::Features
+{
+	class Free_S95 : Command
+	{
+		using Command::Command;
+
+		virtual void OnCall() override
+		{
+			Stats::SetPackedBool(36828, true);
+		}
+	}; 
+	static Free_S95 _Free_S95{"FKarin_S95", "Karin S95", "Free at SSASA."};
+}
