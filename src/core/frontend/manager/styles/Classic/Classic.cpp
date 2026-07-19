3616fc5425ed1937b3f1bc661168549ee380644f
Evolure <ralio2007e@gmail.com>
2026-07-18 21:41:29 -0400

    Update Classic.cpp

--- src/core/frontend/manager/styles/Classic/Classic.cpp
+++ src/core/frontend/manager/styles/Classic/Classic.cpp
@@ -83,74 +83,124 @@ namespace YimMenu
 	static float g_ContentSlide = 0.0f;
 	static decltype(UIManager::GetActiveSubmenu()) g_LastSubmenu{};
 	void RenderClassicTheme()
+{
+	ImGuiIO& io = ImGui::GetIO();
+
+	float windowWidth = *Pointers.ScreenResX / 2.5f;
+	float centerX = (*Pointers.ScreenResX - windowWidth) * 0.5f;
+	float centerY = *Pointers.ScreenResY / 5.0f;
+
+	ImVec2 windowSize(windowWidth, *Pointers.ScreenResY / 2.5f);
+
+	const float animSpeed = 10.0f;
+	float t = io.DeltaTime * animSpeed;
+
+	g_MenuAlpha = LerpFloat(g_MenuAlpha, 1.0f, t);
+	g_MenuSlide = LerpFloat(g_MenuSlide, 0.0f, t);
+
+	const bool useCustomBackground = Menu::customMenuBackground.GetState() && Menu::g_BackgroundTexture.IsValid();
+	ImGui::SetNextWindowBgAlpha(useCustomBackground ? 0.0f : g_MenuAlpha);
+	ImGui::SetNextWindowSize(windowSize,ImGuiCond_FirstUseEver);
+	ImGui::SetNextWindowPos(ImVec2(centerX, centerY + g_MenuSlide), ImGuiCond_FirstUseEver);
+	ImGuiWindowFlags flags =
+		ImGuiWindowFlags_NoTitleBar |
+		ImGuiWindowFlags_NoCollapse;
+
+	if (!ImGui::Begin("##ClassicInputWindow", nullptr, flags))
 	{
-		ImGuiIO& io = ImGui::GetIO();
-		float windowWidth = *Pointers.ScreenResX / 2.5f;
-		float centerX = (*Pointers.ScreenResX - windowWidth) * 0.5f;
-		float centerY = *Pointers.ScreenResY / 5.0f;
-		ImVec2 windowSize(windowWidth,*Pointers.ScreenResY / 2.5f);
-		const float animSpeed = 10.0f;
-		float t = io.DeltaTime * animSpeed;
-		g_MenuAlpha = LerpFloat(g_MenuAlpha, 1.0f, t);
-		g_MenuSlide = LerpFloat(g_MenuSlide, 0.0f, t);
-		ImGui::SetNextWindowBgAlpha(g_MenuAlpha);
-		ImGui::SetNextWindowSize(windowSize, ImGuiCond_FirstUseEver);
-		ImGui::SetNextWindowPos(ImVec2(centerX, centerY + g_MenuSlide),ImGuiCond_FirstUseEver);
-		ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse;
-		if (!ImGui::Begin("##ClassicInputWindow", nullptr, flags))
+		ImGui::End();
+		return;
+	}
+
+	ImVec2 winPos = ImGui::GetWindowPos();
+	ImVec2 winSize = ImGui::GetWindowSize();
+
+	// Custom PNG background
+	if (useCustomBackground)
+	{
+		ImDrawList* draw = ImGui::GetWindowDrawList();
+
+		draw->AddImage(
+			Menu::g_BackgroundTexture.GetTextureID(),
+			winPos,
+			ImVec2(
+				winPos.x + winSize.x,
+				winPos.y + winSize.y
+			),
+			ImVec2(0.0f, 0.0f),
+			ImVec2(1.0f, 1.0f),
+			IM_COL32_WHITE
+		);
+	}
+
+	RenderSnowInWindow(winPos, winSize);
+
+	if (ImGui::BeginChild("##submenus", ImVec2(120, ImGui::GetContentRegionAvail().y), true))
+	{
+		const auto& submenus = UIManager::GetSubmenus();
+		auto activeSubmenu = UIManager::GetActiveSubmenu();
+
+		for (auto& submenu : submenus)
 		{
-			ImGui::End();
-			return;
-		}
-		ImVec2 winPos = ImGui::GetWindowPos();
-		ImVec2 winSize = ImGui::GetWindowSize();
-		RenderSnowInWindow(winPos, winSize);
-		if (ImGui::BeginChild("##submenus",ImVec2(120, ImGui::GetContentRegionAvail().y),true))
-		{
-			const auto& submenus = UIManager::GetSubmenus();
-			auto activeSubmenu = UIManager::GetActiveSubmenu();
-			for (auto& submenu : submenus)
+			if (ImGui::Selectable(
+				submenu->m_Name.data(),
+				submenu == activeSubmenu
+			))
 			{
-				if (ImGui::Selectable(submenu->m_Name.data(),submenu == activeSubmenu))
-				{
-					UIManager::SetActiveSubmenu(submenu);
-					UIManager::SetShowContentWindow(true);
-				}
+				UIManager::SetActiveSubmenu(submenu);
+				UIManager::SetShowContentWindow(true);
 			}
 		}
-		ImGui::EndChild();
-		ImGui::SameLine();
-		ImGui::BeginGroup();
-		ImGui::TextUnformatted("YimMenuV2");
-		if (ImGui::BeginChild("##minisubmenus", ImVec2(0, 50), true))
-		{
-			auto activeSubmenu = UIManager::GetActiveSubmenu();
-			if (activeSubmenu) activeSubmenu->DrawCategorySelectors();
-		}
-		ImGui::EndChild();
-
-		auto activeSubmenu = UIManager::GetActiveSubmenu();
-		if (activeSubmenu != g_LastSubmenu)
-		{
-			g_ContentSlide = 20.0f;
-			g_LastSubmenu = activeSubmenu;
-		}
-		g_ContentSlide = LerpFloat(g_ContentSlide, 0.0f, t);
-		ImGui::Dummy(ImVec2(g_ContentSlide, 0.0f));
-		ImGui::SameLine(0.0f, 0.0f);
-		if (ImGui::BeginChild("##options", ImVec2(0, 0), true))
-		{
-			if (ImFont* optionsFont = UIManager::GetOptionsFont())
-				ImGui::PushFont(optionsFont);
-
-			if (activeSubmenu)
-				activeSubmenu->Draw();
-
-			if (ImGui::GetFont() != ImGui::GetIO().FontDefault)
-				ImGui::PopFont();
-		}
-		ImGui::EndChild();
-		ImGui::EndGroup();
-		ImGui::End();
 	}
+
+	ImGui::EndChild();
+	ImGui::SameLine();
+	ImGui::BeginGroup();
+
+	ImGui::TextUnformatted("YimMenuV2");
+
+	if (ImGui::BeginChild("##minisubmenus", ImVec2(0, 50), true))
+	{
+		auto activeSubmenu = UIManager::GetActiveSubmenu();
+
+		if (activeSubmenu)
+			activeSubmenu->DrawCategorySelectors();
+	}
+
+	ImGui::EndChild();
+
+	auto activeSubmenu = UIManager::GetActiveSubmenu();
+
+	if (activeSubmenu != g_LastSubmenu)
+	{
+		g_ContentSlide = 20.0f;
+		g_LastSubmenu = activeSubmenu;
+	}
+
+	g_ContentSlide = LerpFloat(g_ContentSlide, 0.0f, t);
+
+	ImGui::Dummy(ImVec2(g_ContentSlide, 0.0f));
+	ImGui::SameLine(0.0f, 0.0f);
+
+	if (ImGui::BeginChild("##options", ImVec2(0, 0), true))
+	{
+		bool pushedFont = false;
+
+		if (ImFont* optionsFont = UIManager::GetOptionsFont())
+		{
+			ImGui::PushFont(optionsFont);
+			pushedFont = true;
+		}
+
+		if (activeSubmenu)
+			activeSubmenu->Draw();
+
+		if (pushedFont)
+			ImGui::PopFont();
+	}
+
+	ImGui::EndChild();
+	ImGui::EndGroup();
+	ImGui::End();
+}
 }
